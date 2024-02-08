af217b596b06581718d7b0e3cbdae586dd0bc8e3
Divya <158427965+DivyadDivyad@users.noreply.github.com>
2024-02-08 16:56:56 +0530

    Update GWO.py

--- GWO.py
+++ GWO.py
@@ -1,67 +1,49 @@
 import numpy as np
-import random as rn
-import time
-import numpy.matlib
 
-
-def GWO(val, objfun, x_min ,x_max, itermax):
-    # def BSO(val, objfun, x_min ,x_max, itermax,DATA,V):
-    N, D = val.shape[0], val.shape[1]
-
-    # Initialization of PSO parameters
-    c1 = 2; c2 = 2
-    wmax = 0.9; wmin = 0.1
-    w = np.zeros((itermax, 1))
-    for iter in range(itermax):
-        w[iter] = wmax - ((wmax - wmin) / itermax) * iter  # Inertia weight update
-
-    m = x_min[0]; n = x_max[0]
-    q = (n - m) / (D * 2)
-    Ki = 1
-
-    # Random initialization of position and velocity
-    x = val
-    v = q* np.random.rand(N, D)
-
-    f = objfun( x)
-
-    fgbest = min(f); igbest = np.where(min(f) == fgbest)
-    gbest = x[igbest,:]
-    pbest = x; fpbest = f
-
-    fbst = np.zeros((itermax, 1))
+def GWO(population,objective_function, search_space, population_size, iterations):
+    search_space,D = population.shape[0],population.shape[1]
+    alpha, beta, delta = np.inf, np.inf, np.inf
+    best_sub= np.zeros((iterations, D))
+    convergence = np.zeros((iterations,best_solution))
+    fitness_alpha, fitness_beta, fitness_delta = float('inf'), float('inf'), float('inf')
     ct = time.time()
-    best_sub=np.zeros((itermax, D))
-    # Iterate
-    for it in range(itermax):
-        # Update velocities and position
-        v = w[it] * v +c1 * rn.random() * (pbest - x)+c2 * rn.random()
-        x= x+v
+    for iteration in range(iterations):
+        for i in range(population_size):
+            fitness_i = objective_function(population[i, :])
+            minf = min(fitness_i); iminf = np.where(min(fitness_i) == minf)
+            best_solution[iteration, :] = population[iminf,:]
 
-        for mi in range(N):
-            for mj in range(D):
-                if x[mi, mj] < x_min[mi, mj]:
-                    x[mi, mj] = x_min[mi, mj]
-                else:
-                    if x[mi, mj] > x_max[mi, mj]:
-                        x[mi, mj] = x_max[mi, mj]
+            if fitness_i < fitness_alpha:
+                alpha = population[i, :].copy()
+                fitness_alpha = fitness_i
+            elif fitness_i < fitness_beta:
+                beta = population[i, :].copy()
+                fitness_beta = fitness_i
+            elif fitness_i < fitness_delta:
+                delta = population[i, :].copy()
+                fitness_delta = fitness_i
 
-        f = objfun( x,DATA ,out)
+        a = 2 - 2 * (iteration / iterations)  # Alpha decreases linearly from 2 to 0
 
-        # Find global best and Particle best
-        minf = min(f); iminf = np.where(min(f) == minf)
-        if minf <= fgbest:
-            fgbest = minf; gbest = x[iminf,:]
-            best_sub[it, :] = x[iminf,:]
-            fbst[it] = minf
-        else:
-            fbst[it] = fgbest
-            best_sub[it, :] = gbest
+        for i in range(population_size):
+            r1, r2 = np.random.random(), np.random.random()
+            A1, A2, A3 = 2 * a * r1 - a, 2 * a * r2 - a, 2 * r1
 
-        inewpb = np.where(f <= fpbest)
-        pbest[inewpb,:] = x[inewpb[1],:]
-        fpbest[inewpb] = f[inewpb]
-    ct = time.time() - ct
-    best_fit = fbst[itermax-1]
+            C1 = 2 * np.random.random()
+            D_alpha = abs(C1 * alpha - population[i, :])
+            X1 = alpha - A1 * D_alpha
 
-    return best_fit, fbst[:, 0], np.round(best_sub), ct
+            C2 = 2 * np.random.random()
+            D_beta = abs(C2 * beta - population[i, :])
+            X2 = beta - A2 * D_beta
+
+            C3 = 2 * np.random.random()
+            D_delta = abs(C3 * delta - population[i, :])
+            X3 = delta - A3 * D_delta
+
+            population[i, :] = (X1 + X2 + X3) / 3
+        convergence[iteration] = best_fitness
+    ct = time.time()-ct
+    return best_solution,convergence, best_fitness,ct
+
+
